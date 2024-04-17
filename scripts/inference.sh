1f523ed424277f2dd6a4cf1aaa6ba1d6566ae74d
Luis Bracamontes <braca51e@gmail.com>
2024-04-17 11:52:42 -0400

    working docker image

--- scripts/inference.sh
+++ scripts/inference.sh
@@ -5,9 +5,9 @@ CKPT=$2
 VIDEO=$3
 OUTDIR=${4:-"./examples/res"}
 
-python scripts/demo_inference.py \
+python3 scripts/demo_inference.py \
     --cfg ${CONFIG} \
     --checkpoint ${CKPT} \
     --video ${VIDEO} \
     --outdir ${OUTDIR} \
-    --detector yolo  --save_img --save_video
+    --detector yolo --save_img --vis_fast --sp
